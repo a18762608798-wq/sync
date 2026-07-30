3f4bf4658042b54efce5fbc336715d8adccb8c3d
git_usr <a18762608798@petalmail.com>
2026-07-30 14:49:52 +0800

    组会报告：完善 slides.md 布局与内容（拆图、修正公式、重排顺序）；新增 deploy.yml 部署配置；更新变分方案文档

--- theory/physics/contemporary_physics/quant_comp_note/04_project/variational_param_evolution/variational_qc/get_initial_state.py
+++ theory/physics/contemporary_physics/quant_comp_note/04_project/variational_param_evolution/variational_qc/get_initial_state.py
@@ -9,21 +9,22 @@ def get_initial_state(N, phase_idx=1):
             qc.x(i)
         for i in range(0, N, 2):
             qc.h(i)
-        for i in range(0, N - 2, 2):
+        for i in range(0, N - 1, 2):
             qc.cx(i, i + 1)
     # δ ≠ 0, s = 1
     elif phase_idx == -1:
-        for i in range(1, N - 1, 1):
+        for i in range(0, N, 1):
             qc.x(i)
         for i in range(1, N - 1, 2):
             qc.h(i)
         for i in range(1, N - 2, 2):
             qc.cx(i, i + 1)
+        qc.h(0)
+        qc.cx(0, N - 1)
     # δ = 0, s ≠ ± 1
     elif phase_idx == 0:
         qc.h([0])
         qc.cx([i for i in range(N - 1)], [i for i in range(1, N)])
-        qc.z([0])
         qc.x([2 * i for i in range(N // 2)])
         qc.h([i for i in range(N)])
     else:
