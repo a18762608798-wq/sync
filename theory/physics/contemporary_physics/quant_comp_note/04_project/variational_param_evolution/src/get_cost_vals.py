66cbfc6a080b7a61699530cc905a7e76b7058be1
git_usr <a18762608798@petalmail.com>
2026-08-07 19:01:29 +0800

    save

--- theory/physics/contemporary_physics/quant_comp_note/04_project/variational_param_evolution/src/get_cost_vals.py
+++ theory/physics/contemporary_physics/quant_comp_note/04_project/variational_param_evolution/src/get_cost_vals.py
@@ -1,14 +1,26 @@
+import asyncio
+
+import numpy as np
 from qiskit_aer.primitives import EstimatorV2
+from submit_quark_task import submit_ops_task
 
 QUARK_CHIP = ["Baihua", "Dongling", "Shenglian"]
 
 
-def get_cost_vals(evolution_qc, cost_op, *, chip="qiskit_aer"):
+def get_cost_vals(evolution_qc, cost_op, *, chip="qiskit_aer", options=None):
     if chip == "qiskit_aer":
         return get_cost_vals_by_qiskit_aer(evolution_qc, cost_op)
     elif chip in QUARK_CHIP:
-        pass
-        # return get_cost_vals_by_quark(evolution_qc, cost_op)
+        options = {} if options is None else options
+        correct = options.get("correct", True)
+        target_qubits = options.get("target_qubits", [])
+        return get_cost_vals_by_quark(
+            evolution_qc,
+            cost_op,
+            chip=chip,
+            correct=correct,
+            target_qubits=target_qubits,
+        )
     else:
         raise (ValueError("The chip must be qiskit_aer or in QUARK_CHIP."))
 
@@ -30,3 +42,19 @@ def get_cost_vals_by_qiskit_aer(evolution_qc, cost_op):
     result = job.result()
 
     return result[0].data.evs
+
+
+def get_cost_vals_by_quark(
+    evolution_qc, cost_op, chip="Baihua", correct=True, target_qubits=None
+):
+    result = asyncio.run(
+        submit_ops_task(
+            evolution_qc.decompose(),
+            cost_op,
+            chip=chip,
+            correct=correct,
+            target_qubits=target_qubits,
+        )
+    )
+    cost_val = np.real(result["op_vals"][0])
+    return cost_val
--- theory/physics/contemporary_physics/quant_comp_note/04_project/variational_param_evolution/src/submit_quark_task.py
+++ theory/physics/contemporary_physics/quant_comp_note/04_project/variational_param_evolution/src/submit_quark_task.py
@@ -6,13 +6,22 @@ from qiskit import qasm2
 from quark import Task
 
 
+from assign_quark_task import (
+    get_commute_group,
+    add_meas,
+    get_meas_params,
+    get_pauli_expval_map,
+    get_op_vals,
+)
+
+
 async def submit_quark_task(
     qc,
-    shot_num,
     *,
+    shot_num=1024,
     token=None,
     chip="Baihua",
-    correct=False,
+    correct=True,
     name="my_job",
     target_qubits=None,
 ):
@@ -39,3 +48,44 @@ async def submit_quark_task(
         await asyncio.sleep(10)
         res = tmgr.result(tid)
     return res["count"]
+
+
+async def submit_ops_task(
+    qc,
+    obs,
+    *,
+    shot_num=1024,
+    token=None,
+    chip="Baihua",
+    correct=True,
+    name="my_job",
+    target_qubits=None,
+):
+    groups, meas_pauli_ls = get_commute_group(obs)
+    group_num = len(groups)
+
+    qc, theta, phi = add_meas(qc)
+    binds = get_meas_params(meas_pauli_ls, theta, phi)
+    hists = []
+    for group_idx in range(group_num):
+        bind = {param: vals[group_idx] for param, vals in binds.items()}
+        group_qc = qc.assign_parameters(bind)
+        hist = await submit_quark_task(
+            group_qc,
+            shot_num=shot_num,
+            token=token,
+            chip=chip,
+            correct=correct,
+            name=name,
+            target_qubits=target_qubits,
+        )
+        hists.append(hist)
+    expval_map = get_pauli_expval_map(groups, hists, shot_num)
+    op_vals = get_op_vals(obs, expval_map)
+    result = {
+        "ops": obs,
+        "pauli_groups": groups,
+        "meas_pauli_ls": meas_pauli_ls,
+        "op_vals": op_vals,
+    }
+    return result
