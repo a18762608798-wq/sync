4436df071496c24673bdb03f304d8c828d22a4b3
git_usr <a18762608798@petalmail.com>
2026-09-04 21:49:46 +0800

    更新 RandomMeasAdd：exports、ops_creator 与测试

--- theory/physics/contemporary_physics/quant_comp_note/03_tools_practice/RandomMeasAdd/test/test_modified_method.jl
+++ theory/physics/contemporary_physics/quant_comp_note/03_tools_practice/RandomMeasAdd/test/test_modified_method.jl
@@ -4,27 +4,22 @@ using RandomMeasAdd
 N = 8
 sites = siteinds("Qubit", N)
 group_path = joinpath(
-    @__DIR__, "data", "aer_independent_pidx_-1",
-    "aer_independent_pidx_-1_setting1_settings729_shots1024.npz",
+    @__DIR__, "data", "aer_independent_pidx_1",
+    "aer_independent_pidx_1_setting1_settings729_shots1024.npz",
 )
 sub_group, sub_indices = import_random_group(group_path, sites; permuted_order=[1, 2, 3, 4]);
 sub_shadows = get_dense_shadows(sub_group);
 
-test_index = 2
+test_indices = [1, 2]
 
-if test_index == 1
-    # get expect shadows
+if 1 in test_indices
     reflect_op = create_reflect_op(sub_indices)
     @show linkdims(reflect_op)
-    @show modified_get_expect_shadow(reflect_op, sub_shadows; is_compute_sem=true, is_show_progress=true)
-elseif test_index == 2
-    reflect_op = create_reflect_op(sub_indices)
-    # NOTICE: Which should be equal.
+    # NOTE: Which should be equal.
     @show modified_get_expect_shadow(reflect_op, sub_shadows, is_compute_sem=true, is_show_progress=true)
     @show modified_get_trace_moment(sub_shadows, 1; O=reflect_op, is_compute_sem=true, is_show_progress=true)
-elseif test_index == 3
-    @show modified_get_trace_moment(sub_shadows, 2; is_show_progress=true)
-elseif test_index == 4
+end
+if 2 in test_indices
     @show modified_get_trace_moment(sub_shadows, 2; is_compute_sem=true, is_show_progress=true)
     @show modified_get_purity_shadow(sub_shadows; is_compute_sem=true, is_show_progress=true)
 end
--- theory/physics/contemporary_physics/quant_comp_note/03_tools_practice/RandomMeasAdd/test/test_ops_creator.jl
+++ theory/physics/contemporary_physics/quant_comp_note/03_tools_practice/RandomMeasAdd/test/test_ops_creator.jl
@@ -1,19 +1,22 @@
 using RandomMeasAdd
 
-# settings 
+# settings
 N = 6
 site_indices = siteinds("Qubit", N)
-test_index = 4
 
-if test_index == 1
+test_indices = [1, 2, 3, 4]
+
+if 1 in test_indices
     # reflect op
     reflect_op = create_reflect_op(site_indices)
     @show linkdims(reflect_op)
-elseif test_index == 2
+end
+if 2 in test_indices
     # swap op but adjacent
     adjacent_swap_op = create_adjacent_swap_op(site_indices)
     @show linkdims(adjacent_swap_op)
-elseif test_index == 3
+end
+if 3 in test_indices
     part1 = [1, 2, 3, 4]
     unitary_part_reversal_op = create_unitary_part_reversal_op(
         part1, site_indices; op_type="ITensor"
@@ -22,7 +25,4 @@ elseif test_index == 3
         part1, site_indices; op_type="MPO"
     )
     @show linkdims(unitary_part_reversal_op)
-elseif test_index == 4
-    z_op = create_z_op(site_indices)
-    @show linkdims(z_op)
 end
