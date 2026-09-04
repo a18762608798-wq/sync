8f4c7072af030286a37edd471362fd0e3a366f6f
git_usr <a18762608798@petalmail.com>
2026-09-04 16:36:33 +0800

    RandomMeasAdd 测试数据脚本跟进新版 qmeas API

--- theory/physics/contemporary_physics/quant_comp_note/03_tools_practice/RandomMeasAdd/test/get_data.py
+++ theory/physics/contemporary_physics/quant_comp_note/03_tools_practice/RandomMeasAdd/test/get_data.py
@@ -14,27 +14,25 @@ from qmeas.random import (
     run_random,
 )
 
-from qmeas.models import get_initial_state
-
-test_idx = 1
+from qmeas.models.xxz import get_initial_state
 
 HERE = Path(__file__).resolve().parent
 # aer: independence, pauli
-qc = get_initial_state(8, 4, phase_idx=1)
+qc = get_initial_state(8, phase_idx=1)
 meas_indices = [(2,), (3,), (4,), (5,)]  # Arrange the swap bits together
 setting_runs = [
-    SettingRun(setting_num=3**3, shot_num=1024),
-    SettingRun(setting_num=3**4, shot_num=1024),
+    SettingRun(num_settings=3**4, num_shots=1024),
+    SettingRun(num_settings=3**5, num_shots=1024),
 ]
 aer_opts = AerOptions(method="matrix_product_state", device="CPU", precision="single")
 meas_config = RandomMeasConfig(
     qc=qc,
     setting_runs=setting_runs,
     meas_indices=meas_indices,
-    ensemble="pauli",
+    ensemble="haar",
     runner_opts=aer_opts,
-    output_dir=HERE / "data",
-    name="aer-independence_pauli",
+    output_dir=HERE / "data/aer-shadow/",
+    name="aer-shadow",
 )
 res = asyncio.run(
     run_random(
